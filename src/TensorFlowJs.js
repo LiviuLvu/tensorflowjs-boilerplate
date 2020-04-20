d64d4722ce6c3c4def5cec4908f7a05b36d356c6
LiviuLvu <liviuiancu@protonmail.com>
2020-04-20 12:39:44 +0300

    Clean start point

--- src/TensorFlowJs.js
+++ src/TensorFlowJs.js
@@ -1,9 +1,9 @@
 import * as tfjs from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
-import carsData from "./data/carsData.json";
 
 function TensorFlowJs() {
   console.log("Tensorflow is loaded > ", tfjs);
+  console.log("Tensorflow visor is loaded > ", tfvis);
 
   // required by react component if no elements are created
   return null;
